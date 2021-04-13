9f756585a69ac11eba57646bce2a4e275aab6020
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-04-13 15:14:11 -0700

    user login successful

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const db = require('../models');
+const bcrypt = require("bcrypt");
 
 
 router.get("/", (req, res) => {
     db.User.findAll().then(dbUsers => {
         res.json(dbUsers);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).end();
     })
 })
 
@@ -15,7 +19,28 @@ router.post("/", (req, res) => {
         name: req.body.name,
         password: req.body.password
     }).then(newUser => {
-        res.jason(newUser);
+        res.json(newUser);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).end();
+    })
+})
+
+router.post("/login", (req,res) =>{
+    db.User.findOne({
+        where:{
+            email:req.body.email
+        }
+    }).then(foundUser=>{
+        if(!foundUser){
+            return res.status(404).send("USER NOT FOUND")
+        }
+        //unscrambles encryption to check if passwords match
+        if(bcrypt.compareSync(req.body.password, foundUser.password)){
+            res.status(200).send("login successful")
+        }else {
+            return res.status(403).send("wrong password")
+        }
     })
 })
 
